20627a4cdf1d460290807c1bb9a62da341b796e2
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-11-15 19:47:47 +0530

    Create rotateLL.cpp

---
+++ rotateLL.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        // edge cases 
+        if (head==NULL || head->next ==NULL || k == 0) return head;
+        
+        // compute the length
+        ListNode *cur = head;
+        int len = 1;
+        while (cur->next && ++len) 
+            cur = cur->next;
+        
+        // go till that node
+        cur->next = head;
+        k = k % len; 
+        k = len - k;
+        while (k--) cur = cur->next;
+        
+        // make the node head and break connection 
+        head = cur->next;
+        cur->next = NULL;
+        
+        
+        return head; 
+    }
+};
